799a201dc1112186e141d1e4e1ee80060e452151
robertmaxwilliams <robertmaxwilliams@gmail.com>
2019-08-25 21:15:10 -0400

    Got CLIM sort of working but its basically trash

--- graphics/graphics2.lisp
+++ graphics/graphics2.lisp
@@ -1,9 +1,15 @@
 (ql:register-local-projects) 
 (ql:quickload 'mcclim)
+(ql:quickload 'uiop)
 (ql:quickload 'clim-listener)
 (clim-listener:run-listener :new-process t)
 
+(ql:quickload "clim-examples")   ; Load the system with examples.
+(clim-demo:demodemo) 
+
 (in-package climi)
+
+(in-package :clim-user)
 
 (defparameter *scale-multiplier* 150
   "try modifying me while running!")
@@ -11,6 +17,8 @@
 (defparameter *sleep-time* 0.0001
   "modify and eval to speed or slow the animation, set to `nil' to stop")
 
+(defparameter *sleep-time* nil)
+(defparameter *sleep-time* 0.01)
 (defun cos-animation ()
   (let* ((range (loop for k from 0 to (* 2 pi) by 0.1 collect k)) ; length of 62
          (idx 0)
@@ -30,4 +38,21 @@
        do (progn (sleep *sleep-time*)
                  (if (= 61 idx) (setq idx 0) (incf idx))
                  (redisplay record *standard-output*)))))
-(cos-animation)
+
+;;; Runme! We will need these in a moment.
+(format t "Asd~%")
+(dolist (image-name '("mp-avatar.png"
+		      "vulpes-avatar.png"
+		      "stas-avatar.png"
+		      "suit-avatar.png"
+		      "rainbow-dash-avatar.png"
+		      "chaos-lord-avatar.png"))
+  (progn (format t "~a~%" image-name)
+	 (uiop:run-program (format nil "curl https://common-lisp.net/project/mcclim/static/media/tutorial-1/~A --output /tmp/~A" 
+				   image-name image-name))))
+
+
+
+(clim-listener::define-listener-command (com-ls :name t)
+  ((path 'string))
+  (clim-listener::com-show-directory path))
